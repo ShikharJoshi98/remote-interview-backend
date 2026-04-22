1b6bc97b5c93983ec37ee9a513256ea1a0c972df
ShikharJoshi98 <shikharjoshi89@gmail.com>
2026-04-22 15:29:37 +0530

    errorHandler and apiRoutes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+
 const { serverConfig, dbConfig } = require('./config');
+const errorHandler = require('./middlewares/errorHandler');
+const apiRoutes = require('./routes');
 
 const app = express();
 dbConfig.connectDb();
@@ -11,6 +14,10 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('API is running ...');
 });
+
+app.use('/api', apiRoutes);
+
+app.use(errorHandler);
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Server running on ${serverConfig.PORT}`);
