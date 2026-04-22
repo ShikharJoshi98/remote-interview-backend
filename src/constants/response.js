8982b86d3b9a0f4e7ee3b31268c7a03eec2f26b9
ShikharJoshi98 <shikharjoshi89@gmail.com>
2026-04-22 15:36:12 +0530

    added statusCode and response templates

---
+++ src/constants/response.js
@@ -0,0 +1,11 @@
+const successResponse = (res, data, message, statusCode) => {
+    return res
+        .status(statusCode || 200)
+        .json({
+            success: true,
+            message: message || 'Successfull',
+            data: data || {}
+        });
+};
+
+module.exports = successResponse;
---
+++ src/constants/statusCode.js
@@ -0,0 +1,9 @@
+const STATUS_CODE = {
+    CREATED: 201,
+    OK: 200,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    INTERNAL_SERVER_ERROR: 500
+}
+
+module.exports = STATUS_CODE;
