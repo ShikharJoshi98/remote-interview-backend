6f94483b4b276fe38ec9bcbcda7dae15cace32eb
ShikharJoshi98 <shikharjoshi89@gmail.com>
2026-04-24 02:09:58 +0530

    server checks

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ dbConfig.connectDb();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('API is running ...');
